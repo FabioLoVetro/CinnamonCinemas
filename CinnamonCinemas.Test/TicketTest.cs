16a82ff1320d06245a5077f9031074ad0b57fff4
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-07 20:22:56 +0200

    TicketTest implementation

--- CinnamonCinemas.Test/TicketTest.cs
+++ CinnamonCinemas.Test/TicketTest.cs
@@ -4,28 +4,42 @@ using NUnit.Framework;
 
 namespace CinnamonCinemas.Test
 {
-    public class Test
+    public class TicketTest
     {
         
-
+        Ticket ticket;
         [SetUp]
         public void Setup()
         {
+            ticket = new Ticket();
+            ticket.Movie = "Matrix";
+            ticket.Date = new DateOnly(2022,10,20);
+            ticket.Time = new TimeOnly(15,00);
+            ticket.Seat = "B4";
         }
 
         [Test]
-        public void Test()
+        public void MovieTest()
         {
+            ticket.Movie.Should().Be("Matrix");
         }
 
         [Test]
-        public void Test()
+        public void DateTest()
         {
+            ticket.Date.ToString().Should().Be("20/10/2022");
         }
 
         [Test]
-        public void Test()
+        public void TimeTest()
         {
+            ticket.Time.ToString().Should().Be("15:00");
+        }
+
+        [Test]
+        public void SeatTest()
+        {
+            ticket.Seat.Should().Be("B4");
         }
     }
 }
