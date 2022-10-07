5cdbae0720e05fccb94e68c3a030c6a5919caa62
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-07 20:08:53 +0200

    TicketTest structure

---
+++ CinnamonCinemas.Test/TicketTest.cs
@@ -0,0 +1,31 @@
+using CinnamonCinemas.Model;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CinnamonCinemas.Test
+{
+    public class Test
+    {
+        
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
