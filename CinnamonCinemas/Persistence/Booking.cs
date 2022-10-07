9ddea2404b96e4f405a8cf6fd1845da81c56229c
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-07 19:49:08 +0200

    Booking structure

---
+++ CinnamonCinemas/Persistence/Booking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CinnamonCinemas.Model;
+
+namespace CinnamonCinemas.Persistence
+{
+    public class Booking
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Booking()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// The reservations
+        /// </summary>
+        private List<Ticket> _ticketList
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
