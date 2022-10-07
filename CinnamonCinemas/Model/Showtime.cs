9efbb440551090b3a5b161e9015d27e337574c20
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-07 19:48:13 +0200

    Showtime implementation

---
+++ CinnamonCinemas/Model/Showtime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CinnamonCinemas.Model
+{
+    public class Showtime
+    {
+        private string _movie;
+        private List<DateTime> _dates;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Showtime()
+        {
+            this._dates = new List<DateTime>();
+        }
+
+        /// <summary>
+        /// date and time of the show
+        /// </summary>
+        public List<DateTime> Dates
+        {
+            get => this._dates;
+            set => this._dates = value;
+        }
+
+        /// <summary>
+        /// the movie
+        /// </summary>
+        public string Movie
+        {
+            get => this._movie;
+            set => this._movie = value;
+        }
+    }
+}
