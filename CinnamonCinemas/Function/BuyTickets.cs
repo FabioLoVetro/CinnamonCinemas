0d8f98a80e3dccb3bdce9deeb653fced7b1bec76
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-07 19:49:48 +0200

    BuyTicket structure

---
+++ CinnamonCinemas/Function/BuyTickets.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CinnamonCinemas.Model;
+
+namespace CinnamonCinemas.Function
+{
+    public class BuyTickets
+    {
+        /// <summary>
+        /// Buy a random number of seats (from 1 to 3)
+        /// </summary>
+        /// <param name="showTime">The show time</param>
+        public bool BuyRandomNumberOfSeats(Showtime showTime)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Buy a number of seats specified by user
+        /// </summary>
+        /// <param name="showTime">The show time</param>
+        /// <param name="numberOfSeats">The number of the seats</param>
+        public bool BuyANumberOfSeats(Showtime showTime, int numberOfSeats)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Buy the specified seats by user
+        /// </summary>
+        /// <param name="showTime">The show time</param>
+        /// <param name="seats">The seats to buy</param>
+        public bool BuySpecificSeats(Showtime showTime, string[] seats)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
