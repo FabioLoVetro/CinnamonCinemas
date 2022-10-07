14a9abb2ed89c7fa97c0af765f36b12d56c06378
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-07 19:49:29 +0200

    Ticket structure

---
+++ CinnamonCinemas/Model/Ticket.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CinnamonCinemas.Model
+{
+    public class Ticket
+    {
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Ticket()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// The cinema
+        /// </summary>
+        private string _movie
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// The time
+        /// </summary>
+        private TimeOnly _time
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// The date
+        /// </summary>
+        private DateOnly _date
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// The seat
+        /// </summary>
+        private string _seat
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
