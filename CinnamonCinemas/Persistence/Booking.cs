4906cda3dc7f0847decb5814ac60d39be2cb4596
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-08 11:10:20 +0200

    added cinema object constructor injection

--- CinnamonCinemas/Persistence/Booking.cs
+++ CinnamonCinemas/Persistence/Booking.cs
@@ -8,12 +8,14 @@ namespace CinnamonCinemas.Persistence
 {
     public class Booking
     {
+        private readonly string[] _seats;
         private List<Ticket> _ticketList;
         /// <summary>
         /// Constructor
         /// </summary>
-        public Booking()
+        public Booking(Cinema cinema)
         {
+            this._seats = cinema.Seats;
             this._ticketList = new List<Ticket>();
         }
 
@@ -24,6 +26,14 @@ namespace CinnamonCinemas.Persistence
         {
             get => this._ticketList;
             set => this._ticketList = value;
+        }
+
+        /// <summary>
+        /// The seats of the cinema
+        /// </summary>
+        public string[] Seats
+        {
+            get => this._seats;
         }
     }
 }
