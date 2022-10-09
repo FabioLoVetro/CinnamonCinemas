eff1c0655135e72a25670c1000a1e7b5bd52b6e5
Fabio Lo Vetro <fabio.lovetro@gmail.com>
2022-10-09 14:15:28 +0200

    changed attributes date and time in datetime

--- CinnamonCinemas.Test/BookingTest.cs
+++ CinnamonCinemas.Test/BookingTest.cs
@@ -17,13 +17,11 @@ namespace CinnamonCinemas.Test
             booking = new Booking(new Cinema());
             Ticket matrix = new Ticket();
             matrix.Movie = "Matrix";
-            matrix.Date = new DateOnly(2022, 10, 20);
-            matrix.Time = new TimeOnly(15, 00);
+            matrix.DateTime = new DateTime(2022, 10, 20, 15, 00, 00);
             matrix.Seat = "B4";
             Ticket arrows = new Ticket();
             arrows.Movie = "Arrows";
-            arrows.Date = new DateOnly(2022, 10, 25);
-            arrows.Time = new TimeOnly(19, 00);
+            arrows.DateTime = new DateTime(2022, 10, 25, 19, 00, 00);
             arrows.Seat = "A3";
             booking.TicketList.Add(matrix);
             booking.TicketList.Add(arrows);
